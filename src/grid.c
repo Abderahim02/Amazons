0251b71243d8a4b9b9d2802af6ed68733805b2ee
Abderahim LAGRAOUI <abderahim.lagraoui@bordeaux-inp.fr>
2023-03-24 15:09:31 +0100

    modified the  grid

--- src/grid.c
+++ src/grid.c
@@ -2,13 +2,15 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-struct graph_t * initialize_graph(){
+struct graph_t * initialize_graph(int n){
     gsl_spmatrix_uint *mat = gsl_spmatrix_uint_alloc(n, n);
-    struct graph_t * grid = gsl_spmatrix_uint; 
-    for (size_t i = 0; i < nrows; i++) {
-        for (size_t j = 0; j < ncols; j++) {
+    for (size_t i = 0; i < n; i++) {
+        for (size_t j = 0; j < n; j++) {
             gsl_matrix_set(mat, i, j, NO_DIR);
         }
     }
+    struct graph_t * grid;
+    grid->num_vertices = n*n;
+    grid->t = mat;
     return grid;
 }

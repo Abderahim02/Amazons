9104202132177219a44e3f8a3d27283fdca41d7e
Alloubane Moussa <moussa.alloubane@bordeaux-inp.fr>
2023-03-24 15:40:19 +0100

    changement grid

--- src/grid.c
+++ src/grid.c
@@ -1,11 +1,11 @@
 #include "graph.h"
 #include <stdlib.h>
 #include <stdio.h>
-
+#include "dir.h"
 struct graph_t * initialize_graph(int n){
     gsl_spmatrix_uint *mat = gsl_spmatrix_uint_alloc(n, n);
-    for (size_t i = 0; i < n; i++) {
-        for (size_t j = 0; j < n; j++) {
+    for (unsigned int  i = 0; i < n; i++) {
+        for ( unsigned int j = 0; j < n; j++) {
             gsl_matrix_set(mat, i, j, NO_DIR);
         }
     }
@@ -14,21 +14,19 @@ struct graph_t * initialize_graph(int n){
     grid->t = mat;
     return grid;
 }
-void print_sparse_matrix(gsl_spmatrix_uint *mat) {
-    printf("Sparse matrix:\n");
+void print_matrix(gsl_spmatrix_uint *mat) {
+    printf("matrix:\n");
     printf("size1 = %zu, size2 = %zu\n", mat->size1, mat->size2);
 
-    for (size_t i = 0; i < mat->size1; i++) {
-        for (gsl_spmatrix_uint_const_iter row_iter = gsl_spmatrix_uint_const_row_iterator(mat, i);!gsl_spmatrix_uint_iter_end(row_iter);gsl_spmatrix_uint_const_iter_next(row_iter)) {
-            size_t j = gsl_spmatrix_uint_iter_col(row_iter);
-            unsigned int val = gsl_spmatrix_uint_iter_get(row_iter);
-            printf("(%u)\n",val);
-        }
+// Loop over all pairs of vertices (n*n iterations)
+for (unsigned int i = 0; i < mat->size1; i++) {
+    for (unsigned int j = 0; j < mat->size2; j++) {
+      printf("(%u)\n",gsl_spmatrix_uint_get(mat, i, j));  
     }
-}
+  }
+    }
 void print_graph(struct graph_t * graph);
 int main(){
-struct graph_t * g = initialize_graph(4,4);
-print_graph(g->t);
-    
+struct graph_t * g = initialize_graph(4);
+print_graph(g->t);   
 }

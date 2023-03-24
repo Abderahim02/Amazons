8ea1d2dc95d55a065ddd85cebf0a9a9e60a5ccae
Abderahim LAGRAOUI <abderahim.lagraoui@bordeaux-inp.fr>
2023-03-24 15:36:18 +0100

    hh

--- src/grid.c
+++ src/grid.c
@@ -1,12 +1,12 @@
-#include "graph.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include "grid.h"
 
 struct graph_t * initialize_graph(int n){
     gsl_spmatrix_uint *mat = gsl_spmatrix_uint_alloc(n, n);
     for (size_t i = 0; i < n; i++) {
         for (size_t j = 0; j < n; j++) {
-            gsl_matrix_set(mat, i, j, NO_DIR);
+            gsl_spmatrix_uint_set(mat, i, j, NO_DIR);
         }
     }
     struct graph_t * grid;

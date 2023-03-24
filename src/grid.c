f5071926cae1973cd5ca73207be48afb52ff35c2
Alloubane Moussa <moussa.alloubane@bordeaux-inp.fr>
2023-03-24 16:09:26 +0100

    grid

--- src/grid.c
+++ src/grid.c
@@ -1,13 +1,13 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "grid.h"
-
+#include <stddef.h>
 #include "dir.h"
 
 struct graph_t * initialize_graph(int n){
     gsl_spmatrix_uint *mat = gsl_spmatrix_uint_alloc(n, n);
-    for (size_t i = 0; i < n; i++) {
-        for (size_t j = 0; j < n; j++) {
+    for (unsigned int i = 0; i < n; i++) {
+        for (unsigned int j = 0; j < n; j++) {
             gsl_spmatrix_uint_set(mat, i, j, NO_DIR);
         }
     }
@@ -21,7 +21,7 @@ void print_sparse_matrix(gsl_spmatrix_uint *mat) {
     printf("Sparse matrix:\n");
     printf("size1 = %zu, size2 = %zu\n", mat->size1, mat->size2);
 
-// Loop over all pairs of vertices (n*n iterations)
+// Loop over all gti pairs of vertices (n*n iterations)
 for (unsigned int i = 0; i < mat->size1; i++) {
     for (unsigned int j = 0; j < mat->size2; j++) {
       printf("(%u)\n",gsl_spmatrix_uint_get(mat, i, j));  
@@ -30,8 +30,7 @@ for (unsigned int i = 0; i < mat->size1; i++) {
     }
 
 
-void print_graph(struct graph_t * graph);
 int main(){
-struct graph_t * g = initialize_graph(4,4);
-print_graph(g->t);   
+struct graph_t * g = initialize_graph(4);
+print_sparse_matrix(g->t);   
 }

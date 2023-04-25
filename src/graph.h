2275d05c874944b362a8c87bfa5eb38c7bd7b71f
BOUHAJA Mohammed <Mohammed.Bouhaja@bordeaux-inp.fr>
2023-04-25 17:03:04 +0200

    gsl spmatrix

--- src/graph.h
+++ src/graph.h
@@ -2,8 +2,8 @@
 #define _AMAZON_GRAPH_H_
 
 #include <gsl/gsl_spmatrix.h>
-#include <gsl/gsl_spmatrix_uint.h>
 #include <gsl/gsl_spblas.h>
+#include <gsl/gsl_spmatrix_uint.h>
 #include <stddef.h>
 
 struct graph_t {
